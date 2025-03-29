6df347fd3a09a137f5b67fa2a79e5d16b8f06cf8
Neos21 <neos21@gmail.com>
2025-03-29 22:29:48 +0900

    Design

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,17 +1,32 @@
+<script setup lang="ts">
+
+
+</script>
+
 <template>
-  <v-layout class="rounded rounded-md">
-    <v-app-bar>Miki.js</v-app-bar>
+  <v-app>
+    <v-app-bar :elevation="0" color="black">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+      <v-app-bar-title>Miki.js</v-app-bar-title>
+      <template v-slot:append>
+        <v-btn>Sign-In</v-btn>
+      </template>
+    </v-app-bar>
     
-    <v-navigation-drawer>
-      <v-list>
-        <v-list-item>Miki.js</v-list-item>
-        <v-list-item><v-icon icon="mdi-home" /><RouterLink to="/">Home</RouterLink></v-list-item>
-        <v-list-item><v-icon icon="mdi-account-cog" /><RouterLink to="/admin">Admin</RouterLink></v-list-item>
+    <!-- permanent で表示したままになる -->
+    <v-navigation-drawer permanent color="blue-darken-2">
+      <v-list density="compact">
+        <v-list-item prepend-icon="mdi-home"        link to="/"     >Home</v-list-item>
+        <v-list-item prepend-icon="mdi-account-cog" link to="/admin">Admin</v-list-item>
       </v-list>
     </v-navigation-drawer>
     
-    <v-main>
-      <RouterView />
+    <v-main height="100vh">
+      <v-container>
+        <RouterView />
+      </v-container>
     </v-main>
-  </v-layout>
+  </v-app>
 </template>
