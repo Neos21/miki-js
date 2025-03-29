181aef176dc52c39d295bf2af1ea2f337a3b6639
Neos21 <neos21@gmail.com>
2025-03-29 13:30:49 +0900

    MiAuth

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
+import { v4 } from 'uuid';
+
+const misskeyHostUrl = 'https://misskey.neos21.net';
+
+const redirectToMiAuth = () => {
+  const uuid = v4();
+  localStorage.setItem('uuid', uuid);
+  const params = new URLSearchParams({
+    name: 'Miki.js',
+    callback: window.location.href,  // コールバック URL
+    permission: 'read:account'
+  });
+  const url = `${misskeyHostUrl}/miauth/${uuid}?${params.toString()}`;
+  window.location.href = url;
+};
+
+const getUser = async () => {
+  const uuid = localStorage.getItem('uuid');
+  if(uuid == null || uuid === '') return alert('No Session ID');
+  
+  const response = await fetch(`${misskeyHostUrl}/api/miauth/${uuid}/check`, { method: 'POST' });
+  const json = await response.json();
+  console.log('JSON', json);  // json.token , json.user.id , json.user.username , json.user.name , json.user.avatarUrl
+};
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <h1>Miki.js</h1>
+  <p><button @click="redirectToMiAuth">認証する</button></p>
+  <p><button @click="getUser">ユーザ情報を取得する</button></p>
 </template>
 
 <style scoped>
